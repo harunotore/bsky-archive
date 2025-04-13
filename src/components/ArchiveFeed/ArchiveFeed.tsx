7916ce56d9c6ed7c7775fd4d23cd1ced40bc4a64
Johnny <nguyen.johnly25>
2025-04-13 13:42:05 -0700

    stuff

--- src/components/ArchiveFeed/ArchiveFeed.tsx
+++ src/components/ArchiveFeed/ArchiveFeed.tsx
@@ -1,4 +1,3 @@
-import { $Typed, AppBskyEmbedExternal, AppBskyEmbedImages, AppBskyEmbedRecord, AppBskyEmbedRecordWithMedia, AppBskyEmbedVideo } from "@atproto/api"
 import { FeedViewPost } from "@atproto/api/dist/client/types/app/bsky/feed/defs"
 
 interface ArchiveFeedProps {
@@ -6,25 +5,26 @@ interface ArchiveFeedProps {
 }
 
 export default function ArchiveFeed({ data }: ArchiveFeedProps) {
-
   const dataFilter = data.filter((element: FeedViewPost) => {
     if (element.reason) {
       return false
     } return true
   })
 
-  const authorFeed = dataFilter.map((element: FeedViewPost, index: number) => {
-    console.log(element.post.embed)
-      <div key={index} className="w-45 h-45 overflow-y-hidden">
+  console.log(dataFilter)
+
+  const authorFeed = dataFilter.map((element: FeedViewPost) => {
+    return (
+      <div className="w-45 h-45 overflow-y-hidden">
         {element.post.record.text}
-        {element.post.embed?.$type == 'app.bsky.embed.images#view' &&
-          <div>img</div>
-        }
       </div>
     )
   })
 
+
   return (
-    <div className='flex flex-wrap'>{authorFeed}</div>
+    <div className='flex flex-wrap'>
+      {authorFeed}
+    </div>
   )
 }
